88b30ec1cd1d2521640019d454acfdf7859c4181
roberto-palanga <r.palanga94@gmail.com>
2024-02-19 17:01:27 +0100

    Inserimento delle immagini nello slider

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 
 Bonus:
+
  -1 al click su una thumb, visualizzare in grande l'immagine corrispondente
 
  -2 applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
